216772cdc019440b6471b5c36c939462f8571585
blazej32 <blazej.klepacki1@gmail.com>
2024-03-26 12:30:22 +0100

    code inside html file

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+matches = [
+    {
+        'date': '25.05.2023',
+        'home': 'FC Barcelona',
+        'away': 'Real Madrid',
+        'home_goals': 5,
+        'away_goals': 0
+    },
+    {
+        'date': '02.12.2005',
+        'home': 'Bayern Munich',
+        'away': 'Borussia Dortmund',
+        'home_goals': 2,
+        'away_goals': 2
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
@@ -10,7 +27,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', matches=matches)
 
 
 if __name__ == '__main__':
